f22f14755c6227e130eaad626430ac3876194f12
Monique <moniquerpulcheri@gmail.com>
2025-08-06 22:41:56 -0300

    update

--- cypress/e2e/validacao_cep.cy.js
+++ cypress/e2e/validacao_cep.cy.js
@@ -13,21 +13,20 @@ describe('Validar pagina mercado livre', () => {
         //cy.get('.nav-menu-cp-logged', { timeout: 10000}).should('be.visible').and('contain','Vitória 29090490')//validar CEP foi inserido com sucesso, indicando no topo da página
     })
 
-    it.only('Validar a pagina "ajuda" do mercado livre', () => { 
+    it('Validar a pagina "ajuda" do mercado livre', () => { 
         cy.visit('https://www.mercadolivre.com.br/?msockid=0c03928622f06e7a2b8184bf23ba6f1f')
         cy.get('footer').scrollIntoView({duration:1000})
         cy.get('li.nav-footer-navigation__item').should('be.visible').and('have.length', 9)      
-        cy.get('li.nav-footer-navigation__item').contains('Contato').click()//clicar na opçao "contato"
+        cy.get('.nav-footer-navigation__item > [href="https://www.mercadolivre.com.br/ajuda"]').should('have.text', 'Contato').click()//clicar na opçao "contato"
         cy.url().should('eq','https://www.mercadolivre.com.br/ajuda') //validar que foi redirecionado para a url "contato"
-        cy.get('h1').should('be.visible').contains('Como podemos te ajudar?')//validar que foi redirecionado para a url "contato"
+        cy.get('.search-bar__subtitle').should('be.visible').and('have.text','Como podemos te ajudar?')//validar que foi redirecionado para a url "contato"
         cy.get('a.andes-list__item-anchor').should('be.visible').and('have.length', 8)//validar a quantidade de opções que possui na página
-        cy.get('li').contains('Perguntas frequentes sobre vendas').scrollIntoView({duration:1000}).click()//acessar a opção "Perguntas frequentes sobre vendas"
+        //cy.get('li').should('have.text','Perguntas frequentes sobre vendas').scrollIntoView({duration:1000}).click()//acessar a opção "Perguntas frequentes sobre vendas"
         cy.get('h1.folder__title').should('be.visible', {timeout: 20000}).and('have.text','Vendas')//validar que foi redirecionado para a url "vendas"
         cy.get('li.andes-list__item').should('have.length', 13)//validar a quantidade de opções que possui na página
-
     })
 
-    it('Validar a pagina de "categorias" no mercado livre', () => { 
+    it.only('Validar a pagina de "categorias" no mercado livre', () => { 
         cy.visit('https://www.mercadolivre.com.br/')
         cy.get('.nav-menu-categories-link', {timeout: 10000}).click() 
         cy.get('ul.nav-categs-departments').contains('Supermercado')
